673481421c5573fd37d0b8d6c96bb868266318bc
Manish Mulimani <manish@mobilitas.co>
2014-04-15 23:06:36 +0530

    Adding shim config for non AMD files

--- js/main.js
+++ js/main.js
@@ -4,7 +4,20 @@ require.config({
     backbone   : '//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min',
     jquery     : '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery.min',
     i18n       : '//cdnjs.cloudflare.com/ajax/libs/i18next/1.6.3/i18next.amd.withJQuery-1.6.3.min'
-  }
+  },
+  shim: {
+    underscore:{
+     	exports: '_'
+    },
+    backbone:{
+    	deps:[ 'underscore', 'jquery'],
+        exports: 'Backbone'
+    },
+    jquery: {
+    	exports: '$'
+    }
+  },
+  waitSeconds: 30
 });
  
 require(['app'], function (App) {

5ac38ab0741ec4ddb9b2598354f045243c003383
Manish Mulimani <manish@mobilitas.co>
2014-04-16 06:52:00 +0530

    Removing cdn repository references and using local libs for it work with Cordova

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 require.config({
   paths: {
-    underscore : '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min',
-    backbone   : '//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min',
-    jquery     : '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery.min',
-    i18n       : '//cdnjs.cloudflare.com/ajax/libs/i18next/1.6.3/i18next.amd.withJQuery-1.6.3.min'
+    underscore : 'libs/underscore.min',
+    backbone   : 'libs/backbone.min',
+    jquery     : 'libs/jquery.min',
+    i18n       : 'libs/i18next.amd.withJQuery-1.6.3.min'
   },
   shim: {
     underscore:{
